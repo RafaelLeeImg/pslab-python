360640ee163ba4b856369ce0139fb0d7ab95327a
Rafael Lee <rafaellee.img@gmail.com>
2023-05-19 14:16:37 +0800

    Add driver for hdc1080, won't work

--- pslab/external/hdc1080.py
+++ pslab/external/hdc1080.py
@@ -1,5 +1,5 @@
 import time
-from numpy import int16
+import struct
 from pslab.bus import I2CSlave
 
 
@@ -21,8 +21,8 @@ class HDC1080(I2CSlave):
     REG_HUMIDITY = 0x1
     REG_CONF = 0x2  # 2 bytes
     REG_SN_HIGH = 0xFB
-    REG_SN_MIDDLE = 0xFD
-    REG_SN_LOW = 0xFE
+    REG_SN_MIDDLE = 0xFC
+    REG_SN_LOW = 0xFD
     REG_DEVICE_ID = 0xFF
 
     REG_CONF_RST = 15
@@ -43,14 +43,23 @@ class HDC1080(I2CSlave):
         # self.write([1<<self.REG_CONF_RST],self.REG_CONF)
         # self.write([1<<self.REG_CONF_MODE],self.REG_CONF)
         # status_write = status_write.to_bytes(2, 'big')
-        status_write = b'\x10\x00'
-        print(f'status = {status_write}')
+        status_write = struct.pack(">H", status_write)
+        # status_write = b'\x10\x00'
+        print(f'softwareReset status = {status_write}')
         self.write(status_write, self.REG_CONF)
 
-        status_read = self.read(2, self.REG_CONF_RST)
-        print("status = ", end='')
+    def getStatus(self):
+        status_read = self.read(2, self.REG_CONF)
+        print("getStatus status = ", end='')
         [print(hex(i), end=', ') for i in status_read]
         print('')
+
+    def startAcquire(self):
+        status_write = 0
+        status_write = status_write | (1 << self.REG_CONF_MODE)
+        status_write = struct.pack(">H", status_write)
+        print(f'startAcquire status = {status_write}')
+        self.write(status_write, self.REG_CONF)
 
     def getID(self):
         serial_number = self.read(2, self.REG_SN_HIGH)
@@ -63,7 +72,7 @@ class HDC1080(I2CSlave):
 
     def getTemperature(self):
         self.write([], self.REG_TEMPERATURE)
-        time.sleep(0.01)
+        time.sleep(0.01)  # 10ms
         temperature_raw = self.read(2, self.REG_TEMPERATURE)
         temperature = temperature_raw[0] * 256 + temperature_raw[1]
         temperature = (temperature / 2**16) * 165 - 40
@@ -71,7 +80,7 @@ class HDC1080(I2CSlave):
 
     def getHumidity(self):
         self.write([], self.REG_HUMIDITY)
-        time.sleep(0.01)
+        time.sleep(0.01)  # 10ms
         humidity_raw = self.read(2, self.REG_HUMIDITY)
         humidity = humidity_raw[0] * 256 + humidity_raw[1]
         humidity = humidity / 2**16
